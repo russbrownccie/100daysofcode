8a99ff06390c83beeed40265db4a9e6db7a39287
Russell Brown <78993850+russbrownccie@users.noreply.github.com>
2021-04-29 16:04:11 -0800

    Update main.py

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,4 +1,4 @@
-print ("Welcome to the Band Name Generator!")
+print ("Welcome to the Band Name generator!")
 city = input("What is the name of the city you grew up in?\n")
 pet = input("What is the name of your pet\n")
 print ("your band name could be " + city +" " + pet)
