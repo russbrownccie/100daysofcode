3037e188f267fe17f6461891f87ab54550e5a81f
Russell Brown <78993850+russbrownccie@users.noreply.github.com>
2021-05-10 09:27:21 -0800

    Update oopcoffee.py
    2nd attempt

--- Day 16/oopcoffee.py
+++ Day 16/oopcoffee.py
@@ -2,33 +2,24 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 mymenu = Menu()
-mymachine = CoffeeMaker()
+mymaker = CoffeeMaker()
 mymoney = MoneyMachine()
-coffee_maker_on = True
 
-while coffee_maker_on:
+IsOn = True
 
-    print("What would you like to drink? :" + mymenu.get_items())
-    order_name = input("Choice?: ").lower()
+while IsOn == True:
+    myorder = input (f"What would you like to drink?: {mymenu.get_items()}")
 
-    if order_name == "report":
-        mymachine.report()
+    if myorder.lower() == "report":
+        mymaker.report()
         mymoney.report()
-    elif order_name == "off":
-        print("Turning off Coffeemaker")
-        coffee_maker_on = False
+    elif myorder.lower() == "off":
+        IsOn = False
     else:
-        mydrink = mymenu.find_drink(order_name)
-        print(mydrink)
-        # if order_name == "latte":
-        #     mydrink = MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5)
-        # elif order_name == "espresso":
-        #     mydrink = MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5)
-        # else:
-        #     mydrink = MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
+        mybeverage = mymenu.find_drink(myorder)
 
-        if mymachine.is_resource_sufficient(mydrink):
-            if mymoney.make_payment(mydrink.cost):
-                mymachine.make_coffee(mydrink)
+        if mymaker.is_resource_sufficient(mybeverage) == True:
+            if mymoney.make_payment(mybeverage.cost) == True:
+                mymaker.make_coffee(mybeverage)
+
