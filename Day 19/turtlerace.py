0eab6d902b6e5cdd4cfeec4295110da621064656
Russell Brown <78993850+russbrownccie@users.noreply.github.com>
2021-05-12 15:57:59 -0800

    Update turtlerace.py

--- Day 19/turtlerace.py
+++ Day 19/turtlerace.py
@@ -1,14 +1,14 @@
 
-
+from turtle import Turtle, Screen
 import random
 is_race_on = False
-from turtle import Turtle, Screen
-colors = ["red", "orange", "yellow", "green", "blue", "purple" ]
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_axis = [-70, -40, -10, 20, 50, 80]
 screen = Screen()
 screen.setup(width=500, height=400)
-all_turtles= []
-for item in range (6):
+all_turtles = []
+for item in range(6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(colors[item])
@@ -28,13 +28,11 @@ while is_race_on:
             is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_bet:
-                print (f"You won! - the {winning_color} turtle is the winner!")
+                print(f"You won! - the {winning_color} turtle is the winner!")
             else:
-                print (f"You lost - the {winning_color} turtle is the winner!")
-               
-        rand_distance = random.randint(0,10)
+                print(f"You lost - the {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
-
-
 
 screen.exitonclick()
